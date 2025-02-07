2d79779493e6d9805f79510e9e7faceda5370e25
yassineboucham <yassineboucham2002@gmail.com>
2025-02-07 18:23:20 +0100

    send the index file

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = 3000;
+const __dirname = path.resolve();
 
 app.get("/", (req, res) => {
-    res.send("hello Worlds");
+    res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.listen(port, () => {
