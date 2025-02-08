bde9e5b5fe6967edeb656c7560a28e1311fd1f0d
yassineboucham <yassineboucham2002@gmail.com>
2025-02-08 01:14:49 +0100

    send data to show page and put it in table

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 import express from 'express';
 import path from 'path';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
 const __dirname = path.resolve();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/show", (req, res) => {
+    res.send(`
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Comment</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>${req.body.user_name}</td>
+                <td>${req.body.email}</td>
+                <td>${req.body.comment}</td>
+                </tr>
+            </tbody>
+        </table>
+        `);
 });
 
 app.listen(port, () => {
-    console.log('start listening on the port number' + port);
+    console.log('Server is listening on port ' + port);
 });
